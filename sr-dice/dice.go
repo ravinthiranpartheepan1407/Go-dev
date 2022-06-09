509a6e71ab47a0cd1932fe4ed7390e1270807d06
Cyborn-NFT <general@cybornnft.com>
2022-06-09 08:31:05 +0300

    dice.go

--- sr-dice/dice.go
+++ sr-dice/dice.go
@@ -16,7 +16,73 @@
 
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"math/rand"
+	"time"
+)
+
+func fruit(seeds int) int {
+	return rand.Intn(seeds)
+}
+
+func roll(sides int) int {
+	return rand.Intn(sides) + 1
+}
 
 func main() {
+	rand.Seed(time.Now().UnixMicro())
+	dice, sides := 2, 12
+	rolls := 1
+
+	for i := 1; i <= rolls; i++ {
+		sum := 0
+		for d := 1; d <= dice; d++ {
+			rolled := roll(sides)
+			sum += rolled
+			fmt.Println("Rolled", rolled)
+
+		}
+		fmt.Println("Total Sum", sum)
+
+		switch sum := sum; {
+		case sum == 2 && dice == 2:
+			fmt.Println("Snake Eyes")
+
+		case sum == 7:
+			fmt.Println("Lucky")
+
+		case sum%2 == 0:
+			fmt.Println("Even")
+
+		case sum%2 == 1:
+			fmt.Println("Odd")
+
+		}
+	}
+
+	apple, seeds := 4, 16
+	juice := 2
+	for a := 0; a <= juice; a++ {
+		compute := 0
+		for b := 1; b <= apple; b++ {
+			juiced := fruit(seeds)
+			compute += juiced
+			fmt.Println("Juice Quantity", juiced)
+			fmt.Println("Computed Quantity", compute)
+
+			switch compute := compute; {
+			case compute == 0:
+				fmt.Println("No Juice")
+
+			case compute%2 == 0:
+				fmt.Println("Juice Found")
+
+			case compute%2 == 1:
+				fmt.Println("Half Quantity found")
+
+			}
+		}
+	}
+
 }
